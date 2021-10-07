2dd1b1a06effe7b48255bcfa9981c6a5b73b2c26
dania mustika putri <daniamustikaputri@gmail.com>
2021-10-07 11:39:05 +0700

    Solusi Jurnal 02

---
+++ IMA02.java
@@ -0,0 +1,7 @@
+public class IMA02 {
+	public static void main(String[] args) {
+		System.out.println("Nama : Dania Mustika Putri");
+		System.out.println("NIM : 6706210088");
+		System.out.println("Kelas : 45-04");
+	}
+}
